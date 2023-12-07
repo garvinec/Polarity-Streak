c7f16ef96479fe1521a0d47e7a0a43e731e77ce1
prefacerttp <chan.gar@northeastern.edu>
2023-12-07 14:07:53 -0500

    code review done

--- api/api.py
+++ api/api.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/alph-a-row', methods=['POST'])
 def alph_a_row():
-	data = request.get_json()
-
-	return jsonify({})
+    data = request.get_json(force=True)
+    return jsonify(data)
--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 import darkMode from "./dark-theme.png";
 
 function App() {
+  // Simultaneously shows what the user is typing in the text box
+  const [inputValue, setInputValue] = useState("");
+  const [frontText, setFrontText] = useState("");
+  const [highlightedText, setHighlightedText] = useState("");
+  const [backText, setBackText] = useState("");
+  const [streakCount, setStreakCount] = useState(0);
+  const [highlightedLength, setHighlightedLength] = useState(0);
+  const [firstLetter, setFirstLetter] = useState(0);
+
   useEffect(() => {
     fetch("/alph-a-row", {
       method: "POST",
-      body: JSON.stringify({}),
+      body: JSON.stringify({ inputValue }),
       headers: new Headers({
         "content-type": "application/json",
       }),
@@ -18,37 +27,37 @@ function App() {
         console.error(err);
       }
     });
-  }, []);
+  }, [inputValue]);
 
   // Create a map to pair each alphabet to a number
-  const alpha_num = new Map([
-    ["a", 0],
-    ["b", 1],
-    ["c", 2],
-    ["d", 3],
-    ["e", 4],
-    ["f", 5],
-    ["g", 6],
-    ["h", 7],
-    ["i", 8],
-    ["j", 9],
-    ["k", 10],
-    ["l", 11],
-    ["m", 12],
-    ["n", 13],
-    ["o", 14],
-    ["p", 15],
-    ["q", 16],
-    ["r", 17],
-    ["s", 18],
-    ["t", 19],
-    ["u", 20],
-    ["v", 21],
-    ["w", 22],
-    ["x", 23],
-    ["y", 24],
-    ["z", 25],
-  ]);
+  const alpha_num = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
+    i: 8,
+    j: 9,
+    k: 10,
+    l: 11,
+    m: 12,
+    n: 13,
+    o: 14,
+    p: 15,
+    q: 16,
+    r: 17,
+    s: 18,
+    t: 19,
+    u: 20,
+    v: 21,
+    w: 22,
+    x: 23,
+    y: 24,
+    z: 25,
+  };
 
   // make sure no matter where the user clicks, it focuses on the input box (which is hidden from them)
   const inputRef = useRef(null);
@@ -58,15 +67,6 @@ function App() {
       inputRef.current.focus();
     });
   }, []);
-
-  // Simultaneously shows what the user is typing in the text box
-  const [inputValue, setInputValue] = useState("");
-  const [frontText, setFrontText] = useState("");
-  const [highlightedText, setHighlightedText] = useState("");
-  const [backText, setBackText] = useState("");
-  const [streakCount, setStreakCount] = useState(0);
-  const [highlightedLength, setHighlightedLength] = useState(0);
-  const [firstLetter, setFirstLetter] = useState(0);
 
   // highlight current streak
   useEffect(() => {
@@ -82,9 +82,9 @@ function App() {
     setInputValue(s);
   }
 
-  function isAlpha(str) {
-    return str.length === 1 && str.match(/[a-z]/i);
-  }
+  // function isAlpha(str) {
+  //   return str.length === 1 && str.match(/[a-z]/i);
+  // }
 
   // function that counts the streak
   function CountStreak(s) {
@@ -92,44 +92,27 @@ function App() {
     let currentStreakLength = 0;
     let longestHighlighted = 0;
     let currentHighlightedLength = 0;
-    let prevLetter = "Even";
+    let prevLetter = null;
     let firstLetter = 0;
-    let temp = 0;
+    let tempFirstLetter = 0;
     for (let i = 0; i < s.length; i++) {
-      if (isAlpha(s[i])) {
+      const c = s[i];
+      if (!!alpha_num[c] || alpha_num[c] === 0) {
         // if it's an even alphabet and the previous letter was an even letter
-        if (alpha_num.get(s[i]) % 2 === 0 && prevLetter === "Even") {
+        if (prevLetter === alpha_num[c] % 2) {
           currentStreakLength += 1;
           currentHighlightedLength += 1;
-        }
-        // if it's an even alphabet and the previous letter was an odd letter
-        else if (alpha_num.get(s[i]) % 2 === 0 && prevLetter === "Odd") {
+        } else {
           currentStreakLength = 1;
           currentHighlightedLength = 1;
-          prevLetter = "Even";
-          temp = i;
-        }
-        // if it's an odd alphabet and the previous letter was an odd letter
-        else if (alpha_num.get(s[i]) % 2 !== 0 && prevLetter === "Odd") {
-          currentStreakLength += 1;
-          currentHighlightedLength += 1;
-        }
-        // if it's an odd alphabet and the previous letter was an even letter
-        else if (alpha_num.get(s[i]) % 2 !== 0 && prevLetter === "Even") {
-          currentStreakLength = 1;
-          currentHighlightedLength = 1;
-          prevLetter = "Odd";
-          temp = i;
-        }
-        // should not reach here
-        else {
-          console.log("It shouldn't get here");
+          prevLetter = alpha_num[c] % 2;
+          tempFirstLetter = i;
         }
         // update current streak indices if needed
         if (currentStreakLength > largestStreak) {
           largestStreak = currentStreakLength;
           longestHighlighted = currentHighlightedLength;
-          firstLetter = temp;
+          firstLetter = tempFirstLetter;
         }
         // largestStreak = Math.max(currentStreakLength, largestStreak);
       } else if (s[i] === " ") {
@@ -137,7 +120,8 @@ function App() {
       } else {
         currentStreakLength = 0;
         currentHighlightedLength = 0;
-        temp = i + 1;
+        // tempFirstLetter = ;
+        prevLetter = null;
       }
     }
     setStreakCount(largestStreak);
@@ -162,7 +146,7 @@ function App() {
       </div>
       <div className="main">
         <div className="top">
-          <h1>Polarity Streak!</h1>
+          <h1>Alph-a-row!</h1>
           <div className="instruction">
             Try to make the longest "even" or "odd" streak of letters! Evenness
             for the letters alternates with a being even, b is odd, c is even, d
